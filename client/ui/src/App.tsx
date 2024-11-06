6df042ce28a18d81248feb73f9e62a065f852595
Kane Rogers-Wong <kanerogers@users.noreply.github.com>
2024-11-06 23:20:28 +1100

    First pass at editor (#82)
    * Import types
    
    * Clean up some crimes, make some new ones
    
    * Trash begone
    
    * More trash!
    
    * No need for stupidity now that we can import code like normal people

--- client/ui/src/App.tsx
+++ client/ui/src/App.tsx
@@ -1,25 +1,93 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { AudioPlayer } from "./AudioPlayer";
+import init, { Engine, EngineMode } from "../../pkg/client.js";
 
-function App() {
-  const [count, setCount] = useState(0);
+function App({ engine }: { engine: Engine }) {
+  const initialEngineMode = EngineMode.Edit;
+  const [currentMode, setMode] = useState(initialEngineMode);
+
+  useEffect(() => {
+    console.log("App has been mounted probably");
+  }, []);
 
   const handleClick = () => {
-    const nextValue = (window as unknown).increment(count);
-    setCount(nextValue);
+    const nextMode = nextEngineMode(currentMode);
+    setMode(nextMode);
+    engine.ctx_set_engine_mode(nextMode);
   };
 
   return (
     <>
-      <h1>oh hey triangle this is react, what's up??</h1>
       <div className="card">
-        <button onClick={handleClick}>count from Rust is {count}</button>
+        <p>
+          We are currently in <strong>{getEngineModeText(currentMode)}</strong> mode
+        </p>
+        <button onClick={handleClick}>
+          Switch to {getEngineModeText(nextEngineMode(currentMode))}
+        </button>
         <AudioPlayer />
       </div>
     </>
   );
 }
 
+const getEngineModeText = (mode: EngineMode): string => {
+  switch (mode) {
+    case EngineMode.Play:
+      return "Play";
+    case EngineMode.Edit:
+      return "Edit";
+  }
+};
 
-export default App;
+const nextEngineMode = (mode: EngineMode): EngineMode => {
+  switch (mode) {
+    case EngineMode.Play:
+      return EngineMode.Edit;
+    case EngineMode.Edit:
+      return EngineMode.Play;
+  }
+};
+
+function WasmWrapper() {
+  const [engine, setEngine] = useState<Engine>();
+
+  useEffect(() => {
+    async function load() {
+      try {
+        await init(); // init
+        const engine = Engine.new();
+
+        const tick = (timestamp: number) => {
+          engine.tick(timestamp);
+          window.requestAnimationFrame(tick);
+        };
+
+        window.requestAnimationFrame(tick);
+        window.addEventListener("keydown", (event) => {
+          engine.key_down(event);
+        });
+
+        window.addEventListener("keyup", (event) => {
+          engine.key_up(event);
+        });
+
+        setEngine(engine);
+        console.log("Engine loaded successfully!");
+      } catch (err) {
+        console.error("Unable to load module!", err);
+      }
+    }
+
+    load();
+  }, []);
+
+  if (!engine) {
+    return <div>Loading..</div>;
+  }
+
+  return <App engine={engine} />;
+}
+
+export default WasmWrapper;
--- server/src/main.rs
+++ server/src/main.rs
@@ -2,17 +2,11 @@
 #![cfg(not(target_arch = "wasm32"))]
 
 mod game;
+mod http;
 mod js;
 
 use {
-    anyhow::Result,
-    js::run_js,
-    std::{net::SocketAddr, time::Instant},
-    tokio::{
-        fs,
-        io::{AsyncReadExt, AsyncWriteExt},
-        net::{TcpListener, TcpStream},
-    },
+    std::time::Instant,
     tracing_subscriber::{
         filter::{EnvFilter, LevelFilter},
         layer::SubscriberExt,
@@ -54,89 +48,5 @@ fn main() {
     webbrowser::open("http://localhost:8888").expect("You.. don't have a web browser?");
 
     // start_http_server is !Send, so we need to await it on the current thread
-    let _ = runtime.block_on(start_http_server());
-}
-
-async fn start_http_server() -> Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8888").await?;
-
-    loop {
-        let (stream, addr) = listener.accept().await?;
-        handle_connection(stream, addr).await?;
-    }
-}
-
-async fn handle_connection(mut stream: TcpStream, addr: SocketAddr) -> Result<()> {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).await?;
-
-    let request = String::from_utf8_lossy(&buffer[..]);
-    let mut lines = request.lines();
-    let first_line = lines.next().unwrap_or("");
-    let mut parts = first_line.split_whitespace();
-    let method = parts.next().unwrap_or("");
-    let path = parts.next().unwrap_or("/");
-
-    let response = match method {
-        "GET" => handle_get(path).await?,
-        "POST" => handle_post(path, addr).await?,
-        _ => "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n"
-            .as_bytes()
-            .to_vec(),
-    };
-
-    stream.write_all(&response).await?;
-    stream.flush().await?;
-
-    Ok(())
-}
-
-async fn handle_post(path: &str, addr: SocketAddr) -> Result<Vec<u8>> {
-    let response = run_js(path, addr).await?;
-    Ok(format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: text/plain\r\n\r\n{response}",
-        response.len()
-    )
-    .into())
-}
-
-async fn handle_get(request_path: &str) -> Result<Vec<u8>> {
-    tracing::info!("CLIENT <- GET {request_path}");
-    let request_path = if request_path == "/" {
-        "/index.html"
-    } else {
-        request_path
-    };
-
-    let mut file_path = std::path::PathBuf::from("./client/");
-    file_path.push(&request_path[1..]); // Remove leading '/'
-
-    if !file_path.exists() {
-        tracing::info!("SERVER <- 404");
-        let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n".as_bytes().to_vec();
-        return Ok(response);
-    }
-
-    let contents = fs::read(&file_path).await?;
-    let content_type = if request_path.ends_with(".wasm") {
-        "application/wasm"
-    } else if request_path.ends_with(".js") {
-        "application/javascript"
-    } else if request_path.ends_with(".html") {
-        "text/html"
-    } else {
-        "application/octet-stream"
-    };
-
-    tracing::info!("SERVER -> 200 {file_path:?}");
-
-    Ok(format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: {}\r\n\r\n",
-        contents.len(),
-        content_type
-    )
-    .into_bytes()
-    .into_iter()
-    .chain(contents.into_iter())
-    .collect())
+    let _ = runtime.block_on(http::start_http_server());
 }
