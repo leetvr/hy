4146e22d9ff3c9a73c32355e4a8640af1fe90a09
Lilith <lilith@triplehex.dev>
2024-11-08 19:26:51 +0100

    Omfg this is all I needed to do to get it to work???

--- kibble_ctf/player.tsx
+++ kibble_ctf/player.tsx
@@ -1,7 +1,6 @@
-import PlayerState from "./lib/player";
+import { PlayerState } from "./lib/player";
 
 function updatePlayerState(playerState: PlayerState): PlayerState {
     playerState.position += 1;
-
     return playerState;
 }
