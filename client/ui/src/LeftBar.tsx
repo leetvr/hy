c72c1172369934f7c1c308c7c111eeefae4fc1a7
Patrick Clearwater <patrick@clearwater.id.au>
2024-11-09 14:12:31 +1100

    Editor UI: Left Bar: basic tabbing

--- client/ui/src/LeftBar.tsx
+++ client/ui/src/LeftBar.tsx
@@ -1,6 +1,33 @@
 // The "left bar": the block/entity palettes
+import { useState } from "react";
+import { EngineMode } from "../../pkg/client.js";
+
+enum LeftBarTab {
+    Blocks,
+    Entities,
+    Debug,
+};
+
 export default function LeftBar() {
+    const [currentTab, setCurrentTab] = useState(LeftBarTab.Debug);
+    let theContent = <p>hi - {currentTab}</p>;
+    // TODO: If we ever need to use it for anything else, this tab-bar business
+    // can sensibly be separated into its own component
     return <div className="editor-panel" id="toolbox">
-        <p>Siege chopper, checking in 🚁</p>
+        <div className="tab-bar">
+            <button
+              className={currentTab == LeftBarTab.Blocks ? "tab-on" : ""}
+              onClick={() => { setCurrentTab(LeftBarTab.Blocks); } }
+            >Blocks</button>
+            <button
+              className={currentTab == LeftBarTab.Entities ? "tab-on" : ""}
+              onClick={() => { setCurrentTab(LeftBarTab.Entities); } }
+            >Entities</button>
+            <button
+              className={currentTab == LeftBarTab.Debug ? "tab-on" : ""}
+              onClick={() => { setCurrentTab(LeftBarTab.Debug); } }
+            >Debug</button>
+        </div>
+        {theContent}
     </div>;
 }
--- client/ui/src/RightBar.tsx
+++ client/ui/src/RightBar.tsx
@@ -1,7 +1,8 @@
 // The "right bar": invisible unless an entity is selected, and in that latter
 // case, the properties panel
+
 export default function RightBar() {
     return <div className="editor-panel" id="propbox">
-        <p>no, <b>I</b> am the walrus!</p>;
+        <p>Siege chopper, checking in 🚁</p>
     </div>;
 }
