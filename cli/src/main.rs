92f8d1f790c8c022f831c25fe150a51166f01aa2
Patrick Clearwater <patrick@clearwater.id.au>
2024-11-14 22:39:13 +1100

    WIP - put `clap` in there

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Clone, ValueEnum, Debug)]
+enum CliCommand {
+    /// Create a new World
+    Create,
+    /// Create a new BlockType in the current World
+    BlockType,
+    /// Create a new EntityType in the current World
+    EntityType,
+}
+
+#[derive(Parser)]
+struct Args {
+    /// The broad command to run
+    command: CliCommand,
+    /// The main subject of the command, if any
+    subject: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("command -: {:?}", args.command);
 }
