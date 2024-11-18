f3e894c7d24f7578757f85d1ed8094594ba2d2e8
Patrick Clearwater <patrick@clearwater.id.au>
2024-11-18 22:45:54 +1100

    Improve look of the entity type list in editor

--- client/ui/src/EntityTypeList.tsx
+++ client/ui/src/EntityTypeList.tsx
@@ -1,8 +1,10 @@
-// A list of Entitys
+// A list of EntityTypes
 import { EntityTypeRegistry, EntityType } from "../../pkg/client.js";
+import { useState } from "react";
 
 export default function EntityTypeList({ entityTypeRegistry, setEngineEntityIndex }: { entityTypeRegistry: EntityTypeRegistry, setEngineEntityIndex: (number: number) => void }) {
     const entityTypes = Array.from(entityTypeRegistry.entity_types);
+    const [selectedEntityIndex, setSelectedEntityIndexState] = useState(0);
 
     if (!entityTypes) {
         return <p>Loading entity types...</p>;
@@ -10,12 +12,17 @@ export default function EntityTypeList({ entityTypeRegistry, setEngineEntityInde
 
     const setSelectedEntityType = (index: number) => {
         setEngineEntityIndex(index);
+        setSelectedEntityIndexState(index);
     };
 
     return <div className="entity-button-container">
         {entityTypes.map((entityType: EntityType) => {
+            let isOn = entityType.id == selectedEntityIndex;
             return (
-                <button onClick={(_) => { setSelectedEntityType(entityType.id); }}>{entityType.name}</button>
+                <button
+                    className={"entity-button " + (isOn ? "button-on" : "")}
+                    onClick={(_) => { setSelectedEntityType(entityType.id); }}
+                >{entityType.name}</button>
             );
         })}
     </div>;
