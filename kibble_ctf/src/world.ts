48a931fa5a3753e19e87df03aff7df1bbb217267
Lilith <lilith@triplehex.dev>
2024-11-19 15:31:57 +0100

    Implement ammo (#215)

--- kibble_ctf/src/world.ts
+++ kibble_ctf/src/world.ts
@@ -36,10 +36,6 @@ export const onAddPlayer: WorldOnAddPlayer = (
   playerId: number,
   playerState: PlayerState,
 ): [CustomState, PlayerState] => {
-  // Give this man a gun
-  let gun = hy.spawnEntity(SHOTGUN_TYPE_ID, [0, 0, 0], [0, 0, 0], [0, 0, 0]);
-  hy.anchorEntity(gun, playerId, "hand_right_anchor");
-
   if (worldState.redTeam.length <= worldState.blueTeam.length) {
     playerState.customState.team = "red";
     worldState.redTeam.push(playerId);
