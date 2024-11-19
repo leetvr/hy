3192d8fe7b573059504d89a0c6705f8faf0786fc
Kane Rogers-Wong <kane.m.rogers@gmail.com>
2024-11-19 22:36:41 +1100

    guns guns guns

--- kibble_ctf/dist/bullet.js
+++ kibble_ctf/dist/bullet.js
@@ -1,4 +1,5 @@
-const MAX_LIFETIME = 100;
+const MAX_LIFETIME = 1000;
+export const DT = 0.01666667; // 60HZ
 export const update = (id, entityState) => {
     // If we've travelled a long way, it's time to say goodbye
     if (Math.abs(entityState.position[0]) > 100 ||
@@ -15,6 +16,15 @@ export const update = (id, entityState) => {
         hy.despawnEntity(id);
         return entityState;
     }
+    // Move on our own
     entityState.customState.lifetime = lifetime + 1;
+    entityState.position[0] += entityState.velocity[0] * DT;
+    entityState.position[1] += entityState.velocity[1] * DT;
+    entityState.position[2] += entityState.velocity[2] * DT;
+    hy.getCollisionsForEntity(id).forEach((collision) => {
+        if (collision.collisionTarget === "entity" || collision.collisionTarget === "block") {
+            hy.despawnEntity(id);
+        }
+    });
     return entityState;
 };
---
+++ kibble_ctf/dist/shotgun.js
@@ -0,0 +1,30 @@
+const MAX_RELOAD_TICKS = 30;
+export const update = (id, currentState) => {
+    let reloadTime = currentState.customState.reloadTime;
+    if (typeof reloadTime !== "number") {
+        reloadTime = 0;
+    }
+    currentState.interactions.forEach((interaction) => {
+        if (reloadTime > 0) {
+            return;
+        }
+        moreBalls(interaction);
+        reloadTime = MAX_RELOAD_TICKS;
+    });
+    currentState.customState.reloadTime = reloadTime - 1;
+    return currentState;
+};
+// BALLS
+const moreBalls = (interaction) => {
+    let speed = 50;
+    // If the angle is wrong, don't find out why, just bash it into place
+    let angle = interaction.facingAngle - Math.PI / 2;
+    // TODO: fire multiple balls? maths too hard for kane brain
+    const initialVelocity = [-Math.cos(angle) * speed, 0, Math.sin(angle) * speed];
+    const initialPosition = [
+        interaction.position[0] + -Math.cos(angle),
+        interaction.position[1],
+        interaction.position[2] + Math.sin(angle),
+    ];
+    hy.spawnEntity(2, initialPosition, [0, 0, 0], initialVelocity);
+};
