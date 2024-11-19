1fd3d25f63d15dd6c4316d2f83d7e59c0d931e00
Kane Rogers-Wong <kane.m.rogers@gmail.com>
2024-11-19 20:08:12 +1100

    Despawn them balls

--- kibble_ctf/src/ball.ts
+++ kibble_ctf/src/ball.ts
@@ -1,9 +1,7 @@
 import { EntityState, EntityUpdate, Interaction } from "../lib/hy";
+const MAX_LIFETIME = 1000;
 
-export const update: EntityUpdate = (
-  id: string,
-  entityState: EntityState,
-): EntityState => {
+export const update: EntityUpdate = (id: string, entityState: EntityState): EntityState => {
   // If we've travelled a long way, it's time to say goodbye
   if (
     Math.abs(entityState.position[0]) > 100 ||
@@ -11,7 +9,20 @@ export const update: EntityUpdate = (
     Math.abs(entityState.position[2]) > 100
   ) {
     hy.despawnEntity(id);
+    return entityState;
   }
+
+  let lifetime = entityState.customState.lifetime;
+  if (!lifetime) {
+    lifetime = 0;
+  }
+
+  if (lifetime >= MAX_LIFETIME) {
+    hy.despawnEntity(id);
+    return entityState;
+  }
+
+  entityState.customState.lifetime = lifetime + 1;
 
   return entityState;
 };
